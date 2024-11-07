509a22863155986b2b99ae9048ec2ff9d88a96a1
Hamenba <101323864+Hamenba@users.noreply.github.com>
2024-11-07 04:26:46 -1200

    Add files via upload

---
+++ word.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "addition",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "vulnerable",
+        hint: "Capable of being hurt or damaged"
+    },
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+    {
+        word: "testimony",
+        hint: "Something that serves as evidence"
+    },
+    {
+        word: "exchange",
+        hint: "The act of trading"
+    },
+    {
+        word: "criticism",
+        hint: "A critical comment, article, or essay"
+    },
+    {
+        word: "garden",
+        hint: "Space for planting flower and plant"
+    },
+    {
+        word: "dismiss",
+        hint: "To discard or reject"
+    },
+    {
+        word: "adolescent",
+        hint: "A person in that stage of development"
+    },
+    {
+        word: "republican",
+        hint: "A member of the Republican Party"
+    },
+    {
+        word: "canvas",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "controversy",
+        hint: "A disagreement, argument or quarrel"
+    },
+    {
+        word: "evaluation",
+        hint: "Judging the value of something by making a comparison"
+    },
+    {
+        word: "aggressive",
+        hint: "Vigorously energetic"
+    },
+    {
+        word: "defendant",
+        hint: "The person against whom a lawsuit is filed"
+    },
+    {
+        word: "obligation",
+        hint: "The state of being obligated to do or pay something"
+    },
+    {
+        word: "psychologist",
+        hint: "A specialist in the science of mental life"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    },
+    {
+        word: "sophisticated",
+        hint: "Having or appealing to those having worldly knowledge and refinement"
+    },
+    {
+        word: "analyst",
+        hint: "A person who analyzes or who is skilled in analysis"
+    },
+    {
+        word: "extraordinary",
+        hint: "Far more than usual or expected"
+    },
+    {
+        word: "frustration",
+        hint: "A feeling of annoyance at being hindered or criticized"
+    },
+    {
+        word: "interpret",
+        hint: "To explain or tell the meaning of"
+    },
+]
